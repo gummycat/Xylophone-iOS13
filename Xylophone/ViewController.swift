67332900a606678bc17bc6fc5e29e94314752ed9
Michael Jones <jones.mej@gmail.com>
2023-02-13 20:49:27 -0500

    Coding challenge

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+  
+  var player: AVAudioPlayer!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func keyPressed(_ sender: UIButton) {
+    playSound(note: sender.currentTitle!)
+    sender.alpha = 0.5
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+      sender.alpha = 1.0
     }
-
-
+  }
+  
+  func playSound(note: String) {
+    let url = Bundle.main.url(forResource: note, withExtension: "wav")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
     
-
+  }
 }
-
